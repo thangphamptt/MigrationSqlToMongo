bc6815acfb11cc39c57983ad07061f7bcee0e701
Uy Le <uy.le@orientsoftware.net>
2018-07-14 12:44:09 +0700

    Migrate email

---
+++ MigrateSqlDbToMongoDb/MongoDatabaseHrToolv1/Model/EmailTrackingAttachment.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDatabaseHrToolv1.Model
+{
+	public class EmailTrackingAttachment
+	{
+		[BsonElement("_id")]
+		public ObjectId Id { get; set; }
+		[BsonElement("Id")]
+		public int ExternalId { get; set; }
+		public int EmailTrackingId { get; set; }
+        public string Path { get; set; }
+        public string Filename { get; set; }
+		public long RowID { get; set; }
+	}
+}
