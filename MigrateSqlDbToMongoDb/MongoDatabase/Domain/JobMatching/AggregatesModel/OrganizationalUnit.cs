23958c4cb0ce3ec1c5792b0f08558d76c3781406
Thang Pham <thang.pham@orientsoftware.net>
2018-07-20 11:35:21 +0700

    update organizational unit: add password field

--- MigrateSqlDbToMongoDb/MongoDatabase/Domain/JobMatching/AggregatesModel/OrganizationalUnit.cs
+++ MigrateSqlDbToMongoDb/MongoDatabase/Domain/JobMatching/AggregatesModel/OrganizationalUnit.cs
@@ -5,7 +5,7 @@ namespace MongoDatabase.Domain.JobMatching.AggregatesModel
 	public class OrganizationalUnit : IAggregateRoot, IEntity
     {
         public string Id { get; set; }
-
         public string Name { get; set; }
+        public string Password { get; set; }
     }
 }
