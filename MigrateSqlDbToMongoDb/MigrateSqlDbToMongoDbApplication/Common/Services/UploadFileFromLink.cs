03c389f4c5ac34be61d7638da48b5bb60c395429
Thang Pham <thang.pham@orientsoftware.net>
2018-08-09 14:08:21 +0700

    update code

--- MigrateSqlDbToMongoDb/MigrateSqlDbToMongoDbApplication/Common/Services/UploadFileFromLink.cs
+++ MigrateSqlDbToMongoDb/MigrateSqlDbToMongoDbApplication/Common/Services/UploadFileFromLink.cs
@@ -8,59 +8,58 @@ using System.Threading.Tasks;
 
 namespace MigrateSqlDbToMongoDbApplication.Common.Services
 {
-	public class UploadFileFromLink
-	{
-		private readonly string _connectionString;
-		public UploadFileFromLink(string connectionString)
-		{
-			_connectionString = connectionString;
-			_uploadFileToAzureStorage = new UploadFileToAzureStorage();
-		}
+    public class UploadFileFromLink
+    {
+        private readonly string _connectionString;
+        public UploadFileFromLink(string connectionString)
+        {
+            _connectionString = connectionString;
+            _uploadFileToAzureStorage = new UploadFileToAzureStorage();
+        }
 
-		private UploadFileToAzureStorage _uploadFileToAzureStorage;
+        private UploadFileToAzureStorage _uploadFileToAzureStorage;
 
-		public async Task<string> GetAttachmentPathAsync(AttachmentFileModel attachment, string newPath, string containerFolder)
-		{
-			using (var client = new HttpClient())
-			{
+        public async Task<string> GetAttachmentPathAsync(AttachmentFileModel attachment, string newPath, string containerFolder)
+        {
+            using (var client = new HttpClient())
+            {
 
-				if (!string.IsNullOrEmpty(attachment.Path))
-				{
-					var attachmentUrl = new Uri(attachment.Path);
+                if (!string.IsNullOrEmpty(attachment.Path))
+                {
+                    try
+                    {
+                        var attachmentUrl = new Uri(attachment.Path);
+                        HttpResponseMessage result = client.GetAsync(attachmentUrl).Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            var contentData = await result.Content.ReadAsByteArrayAsync();
+                            using (var stream = new MemoryStream(contentData))
+                            {
+                                var contentType = MimeMapping.MimeUtility.GetMimeMapping(attachment.Name);
 
-					try
-					{
-						HttpResponseMessage result = client.GetAsync(attachmentUrl).Result;
-						if (result.IsSuccessStatusCode)
-						{
-							var contentData = await result.Content.ReadAsByteArrayAsync();
-							using (var stream = new MemoryStream(contentData))
-							{
-								var contentType = MimeMapping.MimeUtility.GetMimeMapping(attachment.Name);
+                                var fileAzureStorageModel = new FileAzureStorageModel()
+                                {
+                                    ContainerName = containerFolder,
+                                    ContentType = contentType,
+                                    FileName = attachment.Name,
+                                    Stream = stream,
+                                    StorageConnectionString = _connectionString,
+                                    FileUrl = newPath
+                                };
 
-								var fileAzureStorageModel = new FileAzureStorageModel()
-								{
-									ContainerName = containerFolder,
-									ContentType = contentType,
-									FileName = attachment.Name,
-									Stream = stream,
-									StorageConnectionString = _connectionString,
-									FileUrl = newPath
-								};
+                                attachment.Path = await _uploadFileToAzureStorage.UploadFileAsync(fileAzureStorageModel);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Cannot upload file with path: {attachment.Path}\n", ex);
+                        return string.Empty;
+                    }
+                }
 
-								attachment.Path = await _uploadFileToAzureStorage.UploadFileAsync(fileAzureStorageModel);
-							}
-						}
-					}
-					catch (Exception ex)
-					{
-                        Console.WriteLine(ex);
-						throw ex;
-					}
-				}
-
-				return attachment.Path;
-			}
-		}
-	}
+                return attachment.Path;
+            }
+        }
+    }
 }
